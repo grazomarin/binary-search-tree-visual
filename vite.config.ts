b5211caeafc8cc5ae05694e91a0a229d124cdb6f
Kamran Huseynov <huseynovk355@gmail.com>
2023-07-20 09:37:41 +0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), mkcert()],
+	server: {
+		https: true,
+	},
+	test: {
+		watch: true,
+		coverage: {
+			reporter: ['text-summary'],
+		},
+	},
+});
