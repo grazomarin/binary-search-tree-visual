f143a3f3e63e7b34132be19f48c680c0940aaa69
Kamran Huseynov <huseynovk355@gmail.com>
2023-07-24 12:32:22 +0400

    feat: add prettyPrint func, add buildTree func and its corresponding test

--- src/binary-tree.ts
+++ src/binary-tree.ts
@@ -15,7 +15,39 @@ export const NodeFactory = (value: number): Node => {
 export const Tree = () => {
 	let tree: Node | null = null;
 
+	function prettyPrint(node = tree, prefix = '', isLeft = true) {
+		if (node === null) {
+			throw new Error('there is no tree to pretty print');
+		}
+		if (node.right !== null) {
+			prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+		}
+		console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+		if (node.left !== null) {
+			prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+		}
+	}
+
+	function buildTree(value: number[]) {
+		if (value.length === 0) return null;
+		const sorted = [...new Set(value.sort((a, b) => a - b))];
+		const middleIndex = Math.floor((sorted.length - 1) / 2);
+		tree = NodeFactory(sorted[middleIndex]);
+		tree.left = buildRecursion(sorted.slice(0, middleIndex));
+		tree.right = buildRecursion(sorted.slice(middleIndex + 1));
+		function buildRecursion(array: number[]): Node | null {
+			if (array.length === 0) return null;
+			const middleIndex = Math.floor((array.length - 1) / 2);
+			const node = NodeFactory(array[middleIndex]);
+			node.left = buildRecursion(array.slice(0, middleIndex));
+			node.right = buildRecursion(array.slice(middleIndex + 1));
+			return node;
+		}
+	}
+
 	return {
 		getTree: () => tree,
+		prettyPrint,
+		buildTree,
 	};
 };
--- src/tests/binary-tree.test.ts
+++ src/tests/binary-tree.test.ts
@@ -2,4 +2,25 @@ import { describe, it, expect } from 'vitest';
 import { NodeFactory, Tree } from '../binary-tree';
 
 describe('Binary Search Tree FC', () => {
+	it('creates a balanced tree', () => {
+		const bst = Tree();
+		bst.buildTree([2, 1, 1, 4, 3, 5]);
+		expect(bst.getTree()).toMatchObject({
+			value: 3,
+			left: {
+				value: 1,
+				left: null,
+				right: {
+					value: 2,
+					left: null,
+					right: null,
+				},
+			},
+			right: {
+				value: 4,
+				left: null,
+				right: { value: 5, left: null, right: null },
+			},
+		});
+	});
 });
